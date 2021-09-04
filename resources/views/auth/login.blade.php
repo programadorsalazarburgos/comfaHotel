2f63f3ee911f98f886913c1ae0ad4d1e0d6dec63
Johan <sif@idartes.gov.co>
2021-09-04 11:33:14 -0500

    cambios login css

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,31 +2,24 @@
 
 @section('content')
 
+
 <form  method="POST" action="{{ route('login') }}" name="login-form" id="user-login-form">
     {{ csrf_field() }}
     <!-- <img id="logologin" src="{{ asset('Images/logo-nanaimo.svg') }}" > -->
-  <h2 class="title">Bienvenido</h2>
-        <div class="input-div one">
-           <div class="i">
-              <i class="fas fa-user"></i>
-           </div>
+  <h2 class="title">Login</h2>
+        <div class="input-div one">        
            <div class="div{{$errors->has('usuario' ? 'is-invalid' : '')}}">
-              <h5>Usuario</h5>
-              <input type="text" value="{{old('usuario')}}" name="usuario" id="usuario" class="input">
+              <input type="text"  class="form-control" value="{{old('usuario')}}" name="usuario" id="usuario" class="input">
            </div>
         </div>
-        <div class="input-div pass">
-           <div class="i">
-              <i class="fas fa-lock"></i>
-           </div>
+        <div class="input-div pass">         
            <div class="div">
-              <h5>Password</h5>
               <input class="input" type="password" name="password">
            </div>
         </div>
       <br>
-      {!!$errors->first('usuario','<span class="invalid-feedback">:message</span>')!!}
-        <input type="submit" class="btn" value="INICIAR">
+      {!!$errors->first('usuario','<span class="invalid-feedback">:message</span>')!!} <br>
+      <button type="submit">Iniciar Sesión</button>
       </form>
 
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,76 +1,180 @@
+<!DOCTYPE html>
+<html>
 
-<html lang="es">
 <head>
-   <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <style>
+    @import url('https://fonts.googleapis.com/css?family=Montserrat:400,800');
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="UiuzJblUxq58OYBfS2W6Kh22wuke3odzr4aLaERe">
-    <title>HOTEL PISOJE</title>
+    * {
+      box-sizing: border-box;
+    }
 
-    <link rel="stylesheet" type="text/css" href="http://hotelpms.cloud/pms/css/style.css">
-  	<link href="https://fonts.googleapis.com/css?family=Poppins:600&display=swap" rel="stylesheet">
-  	<script src="https://kit.fontawesome.com/a81368914c.js"></script>
-    <style>
-      #logologin{
-        height:150px;
-        margin-bottom: 30px;
-        align-self: center;
-      }
-      .containerright{
-        right:0;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        padding-top: 188px;
-      }
-      .containerleft{
-        width:40%;
-        height:100%;
-        float: left;
-        border-right: 6px solid #c31e24;
-        background-image:url('/Images/login-bg01.jpeg');
-        background-size: cover;
-        background-repeat: no-repeat;
+    body {
+      background: #7cacd5;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      flex-direction: column;
+      font-family: 'Montserrat', sans-serif;
+      height: 100vh;
+    }
 
-      }
-      .btn {
-        background-image: linear-gradient(to right, #dc0c0c, #960b00);
-      }
+    h1 {
+      font-weight: bold;
+      margin: 0;
+    }
+
+    p {
+      font-size: 14px;
+      font-weight: 100;
+      line-height: 20px;
+      letter-spacing: 0.5px;
+      margin: 20px 0 30px;
+    }
+
+    span {
+      font-size: 12px;
+    }
+
+    a {
+      color: #333;
+      font-size: 14px;
+      text-decoration: none;
+      margin: 15px 0;
+    }
+
+    button {
+      border-radius: 20px;
+      border: 1px solid #FF4B2B;
+      background-color: #FF4B2B;
+      color: #FFFFFF;
+      font-size: 12px;
+      font-weight: bold;
+      padding: 12px 45px;
+      letter-spacing: 1px;
+      text-transform: uppercase;
+      transition: transform 80ms ease-in;
+    }
+
+    form {
+      background-color: #FFFFFF;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      flex-direction: column;
+      padding: 0 50px;
+      height: 100%;
+      text-align: center;
+    }
+
+    input {
+      background-color: #eee;
+      border: none;
+      padding: 12px 15px;
+      margin: 8px 0;
+      width: 100%;
+    }
+
+    .container {
+      background-color: #fff;
+      border-radius: 10px;
+      box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
+      position: relative;
+      overflow: hidden;
+      width: 768px;
+      max-width: 100%;
+      min-height: 480px;
+    }
+
+    .form-container {
+      position: absolute;
+      top: 0;
+      height: 100%;
+    }
+
+    .log-in-container {
+      left: 0;
+      width: 50%;
+      z-index: 2;
+    }
 
 
-      /*Responsive*/
-      @media only screen and (max-width: 800px) {
-  div.containerleft {
-    display: none;
-  }
-  .containerright{
-    right:0;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    padding-top: 188px;
-    padding-left: 10%;
-    padding-right: 10%;
-  }
-  #logologin{
-    width: 80%;
-    margin-bottom: 30px;
-    align-self: center;
-  }
-}
-   </style>
- </head>
- <body>
- <div class="containerleft">
+    .overlay-container {
+      position: absolute;
+      top: 0;
+      left: 50%;
+      width: 50%;
+      height: 100%;
+    }
 
- </div>
-   <div class="containerright">
-    <div class="login-content">
+
+    .overlay {
+      background: #FF416C;
+      background: -webkit-linear-gradient(to right, #FF4B2B, #FF416C);
+      background: linear-gradient(to right, #fafafa, #1299da);
+      background-repeat: no-repeat;
+      background-size: cover;
+      background-position: 0 0;
+      color: #FFFFFF;
+      position: relative;
+      left: -100%;
+      height: 100%;
+      width: 200%;
+    }
+
+    .overlay-panel {
+      position: absolute;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      flex-direction: column;
+      padding: 0 40px;
+      text-align: center;
+      top: 0;
+      height: 100%;
+      width: 50%;
+    }
+
+
+    .overlay-right {
+      right: 0;
+    }
+
+
+    .social-container {
+      margin: 50px 0;
+    }
+
+    .social-container a {
+      border: 1px solid #DDDDDD;
+      border-radius: 50%;
+      display: inline-flex;
+      justify-content: center;
+      align-items: center;
+      margin: 0 5px;
+      height: 40px;
+      width: 40px;
+    }
+  </style>
+  <title>Login Form Using HTML And CSS Only</title>
+</head>
+
+<body>
+  <div class="container" id="container">
+    <div class="form-container log-in-container">
     @yield('content')
+    </div>
+    <div class="overlay-container">
+      <div class="overlay">
+        <div class="overlay-panel overlay-right">
+          <h1>Login Hotel Pisojé - Comfacauca</h1>
+          <p>Ingresa tus datos para iniciar sesión al sistema administrativo del hotel de comfacauca.</p>
+        </div>
+      </div>
+    </div>
   </div>
-</div>
-
-<script type="text/javascript" src="http://hotelpms.cloud/pms/js/main.js"></script>
 </body>
+
 </html>
